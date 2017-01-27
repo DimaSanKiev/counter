6d5d6ab9d5d39638e10daf37ba67d02f58340bd8
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2017-01-28 00:33:53 +0200

    Add H2 database and Hibernate configuration

---
+++ src/main/java/io/dimasan/config/H2DataSourceConfig.java
@@ -0,0 +1,33 @@
+package io.dimasan.config;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
+
+
+@Configuration
+public class H2DataSourceConfig implements TransactionManagementConfigurer {
+
+    @Bean
+    public DataSource dataSource() {
+        DataSource dataSource = new DataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:mem:counterdb");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("");
+        return dataSource;
+    }
+
+    @Bean
+    public PlatformTransactionManager txManager() {
+        return new DataSourceTransactionManager(dataSource());
+    }
+
+    @Override
+    public PlatformTransactionManager annotationDrivenTransactionManager() {
+        return txManager();
+    }
+}
---
+++ src/main/java/io/dimasan/config/HibernateJpaConfig.java
@@ -0,0 +1,70 @@
+package io.dimasan.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.persistenceunit.PersistenceUnitManager;
+import org.springframework.orm.jpa.vendor.AbstractJpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Configuration
+public class HibernateJpaConfig {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private PersistenceUnitManager persistenceUnitManager;
+
+    private Map<String, String> properties = new HashMap<>();
+
+    public HibernateJpaConfig() {
+        properties.put("hibernate.hbm2ddl.auto", "create-drop");
+//        properties.put("hibernate.show_sql", "true");
+        properties.put("hibernate.implicit_naming_strategy", "org.hibernate.boot.model.naming.ImplicitNamingStrategyComponentPathImpl");
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        return new JpaTransactionManager();
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        AbstractJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setShowSql(true);
+        vendorAdapter.setDatabase(Database.H2);
+        vendorAdapter.setDatabasePlatform("H2");
+        vendorAdapter.setGenerateDdl(true);
+        return vendorAdapter;
+    }
+
+    @Bean
+    public EntityManagerFactoryBuilder entityManagerFactoryBuilder(JpaVendorAdapter jpaVendorAdapter) {
+        EntityManagerFactoryBuilder builder = new EntityManagerFactoryBuilder(
+                jpaVendorAdapter, properties, this.persistenceUnitManager
+        );
+        builder.setCallback(null);
+        return builder;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(EntityManagerFactoryBuilder builder) {
+        Map<String, Object> vendorProperties = new LinkedHashMap<>();
+        vendorProperties.putAll(properties);
+
+        return builder.dataSource(this.dataSource).packages("io.dimasan.domain")
+                .properties(vendorProperties).jta(false).build();
+    }
+}
